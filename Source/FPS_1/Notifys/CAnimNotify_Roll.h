f495073df29cac88e3422d22a0c622ce40adce87
msk0000 <157696189+msk0000@users.noreply.github.com>
2024-05-03 23:58:36 +0900

    0503_Roll_Notify

---
+++ Source/FPS_1/Notifys/CAnimNotify_Roll.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "CAnimNotify_Roll.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPS_1_API UCAnimNotify_Roll : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+};
